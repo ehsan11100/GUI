39e7a632f66f86171c88f5b20c2f5ed003d8d8d2
WinPlay02 <winplay02_gh@woberlaender.de>
2019-01-02 16:33:20 +0100

    Kommentierung des Codes

--- GUI/src/GUITaschenrechner/GuiTaschenrechner.java
+++ GUI/src/GUITaschenrechner/GuiTaschenrechner.java
@@ -10,77 +10,112 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
+// Elternklasse JFrame
 public class GuiTaschenrechner extends JFrame {
 
-	private JTextField labelOperand;
+	// Koponenten
 	private JLabel labelOperand1;
 	private JLabel labelOperand2;
 
+	// Textfelder
 	private JTextField fieldOperand1;
 	private JTextField fieldOperand2;
+	private JTextField fieldOperand;
 
 	private JButton buttonRechnen;
 
 	private String operator;
 
+	/**
+	 * Kostruktor
+	 * 
+	 * @param titel    Titel des Frames
+	 * @param operator Voreingestellter Operator
+	 */
 	public GuiTaschenrechner(String titel, String operator) {
-
 		this.operator = operator;
 
+		// Titel setzen
 		setTitle(titel);
+		// Frame wird beendet, wenn auf X gedrückt wird
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		// Layout der ContentPane des Frames
 		setLayout(new FlowLayout());
 
+		// Größe des Frames
 		setSize(300, 150);
+		// Die Größe des Frames kann vom Benutzer geändert werden
 		setResizable(true);
 
+		// Komponenten Initialisieren, dazu mehr weiter unten
 		initComponents();
 
+		// Komponenten werden dem Frame hinzugefügt
 		add(labelOperand1);
 		add(labelOperand2);
 		add(fieldOperand1);
-		add(labelOperand);
+		add(fieldOperand);
 		add(fieldOperand2);
 		add(buttonRechnen);
 
+		// Frame wird in der Mitte des Bildschirms angezeigt
 		setLocationRelativeTo(null);
+		// Frame wird sichtbar gemacht
 		setVisible(true);
 
 	}
 
+	/**
+	 * Komponenten initialisieren
+	 */
 	private void initComponents() {
-
-		labelOperand = new JTextField(operator);
+		// Beschreibungen der Textfelder
 		labelOperand1 = new JLabel("1. Zahl                      ");
 		labelOperand2 = new JLabel("2. Zahl                      ");
 
+		// 1. Zahl, vom Benutzer angegeben
 		fieldOperand1 = new JTextField(10);
+		// 2. Zahl, vom Benutzer angegeben
 		fieldOperand2 = new JTextField(10);
+		// Operator kann vom Benutzer bestimmt werden
+		fieldOperand = new JTextField(operator);
 
+		// Button mit dem Titel "Rechnen"
 		buttonRechnen = new JButton("Rechnen");
+		// Button reagiert auf einen Mausklick
 		buttonRechnen.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
-				float op1 = Float.parseFloat(fieldOperand1.getText());
-				float op2 = Float.parseFloat(fieldOperand2.getText());
-
+				float op1;
+				float op2;
+				// Wenn etwas anderes als Zahlen oder gar nichts eingegeben werden, wird nicht
+				// gerechnet
+				try {
+					op1 = Float.parseFloat(fieldOperand1.getText());
+					op2 = Float.parseFloat(fieldOperand2.getText());
+				} catch (NumberFormatException exception) {
+					JOptionPane.showMessageDialog(GuiTaschenrechner.this, "Keine Zahlen", "Error",
+							JOptionPane.ERROR_MESSAGE);
+					return;
+				}
 				float ergibniss;
 
-				if (labelOperand.getText().equals("+")) {
+				// Operator bestimmen
+				if (fieldOperand.getText().equals("+")) {
 					ergibniss = op1 + op2;
-				} else if (labelOperand.getText().equals("-")) {
+				} else if (fieldOperand.getText().equals("-")) {
 					ergibniss = op1 - op2;
-				} else if (labelOperand.getText().equals("/")) {
+				} else if (fieldOperand.getText().equals("/")) {
 					ergibniss = op1 / op2;
-				} else if (labelOperand.getText().equals("*")) {
+				} else if (fieldOperand.getText().equals("*")) {
 					ergibniss = op1 * op2;
 				} else {
 					ergibniss = 666;
 				}
-
+				// Ergebnis wird in Dialog ausgegeben
 				JOptionPane.showMessageDialog(GuiTaschenrechner.this, "Ergebnis: " + ergibniss, "",
 						JOptionPane.INFORMATION_MESSAGE);
+				// Zahlen werden resettet
 				fieldOperand1.setText("");
 				fieldOperand2.setText("");
 
--- GUI/src/GUITaschenrechner/TS.java
+++ GUI/src/GUITaschenrechner/TS.java
@@ -2,8 +2,15 @@ package GUITaschenrechner;
 
 public class TS {
 
+	/**
+	 * Einstiegspunkt
+	 * 
+	 * @param args Parameter, von der Kommandozeile
+	 */
 	public static void main(String[] args) {
+		// Frame wird erstellt
 		GuiTaschenrechner meinTaschenrechner = new GuiTaschenrechner("Project X", "*");
+		// Frame wird sichbar gemacht
 		meinTaschenrechner.setVisible(true);
 	}
 }
