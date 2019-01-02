4c99524a803556c049716e2ce19ab9a4bd73c0fb
WinPlay02 <winplay02_gh@woberlaender.de>
2019-01-02 18:21:15 +0100

    Anpassung des Designs

--- GUI/src/GUITaschenrechner/TS.java
+++ GUI/src/GUITaschenrechner/TS.java
@@ -1,5 +1,8 @@
 package GUITaschenrechner;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class TS {
 
 	/**
@@ -8,6 +11,13 @@ public class TS {
 	 * @param args Parameter, von der Kommandozeile
 	 */
 	public static void main(String[] args) {
+		// Anpassung des Designs an das Betriebssystem
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 		// Frame wird erstellt
 		GuiTaschenrechner meinTaschenrechner = new GuiTaschenrechner("Project X", "*");
 		// Frame wird sichbar gemacht
