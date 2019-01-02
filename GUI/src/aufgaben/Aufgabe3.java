1e5e43c07bb76f1f606166b6931cc140488e01c2
WinPlay02 <winplay02_gh@woberlaender.de>
2019-01-02 17:37:16 +0100

    7 Aufgaben für Swing

---
+++ GUI/src/aufgaben/Aufgabe3.java
@@ -0,0 +1,22 @@
+package aufgaben;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * Füge ein Label hinzu, dass die Aufschrift "Dies ist ein Label" trägt.
+ */
+public class Aufgabe3 {
+	public static void main(String... args) {
+		JFrame jframe1 = new JFrame();
+		jframe1.setSize(500, 500);
+		jframe1.setTitle("GUI mit Swing");
+		JLabel label1 = new JLabel("Dies ist ein Label");
+		JPanel panel1 = new JPanel();
+		panel1.add(label1);
+		jframe1.add(panel1);
+		jframe1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe1.setVisible(true);
+	}
+}
---
+++ GUI/src/aufgaben/Aufgabe7.java
@@ -0,0 +1,38 @@
+package aufgaben;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * Sorge dafür, dass der Text des Labels (von 3.) jetzt den Inhalt des
+ * Textfeldes (4.) wiedergibt.
+ */
+public class Aufgabe7 {
+	public static void main(String... args) {
+		JFrame jframe1 = new JFrame();
+		jframe1.setSize(500, 500);
+		jframe1.setTitle("GUI mit Swing");
+		JLabel label1 = new JLabel("Dies ist ein Label");
+		JTextField textfield1 = new JTextField(20);
+		JPanel panel1 = new JPanel();
+		JButton button1 = new JButton("Button");
+		button1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				label1.setText(textfield1.getText());
+			}
+		});
+		panel1.add(textfield1);
+		panel1.add(label1);
+		panel1.add(button1);
+		jframe1.add(panel1);
+		jframe1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe1.setVisible(true);
+	}
+}
